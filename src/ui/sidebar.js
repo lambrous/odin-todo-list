c07b69bc5b4a7de8e7ac445c74edd6aa25a35659
Lambrous <lambrous@outlook.ph>
2024-05-07 17:37:59 +0800

    Update sidebar icons and styles
    - Add material design icons to sidebar buttons for clarity and
      consistency.
    - Adjust sidebar icon sizes and text styles to enhance readability
      and visual appeal.

--- src/ui/sidebar.js
+++ src/ui/sidebar.js
@@ -5,15 +5,19 @@ export function createProjectElement(project, onClick) {
 	projectElement.classList.add("nav-item");
 
 	const icon = document.createElement("span");
-	icon.textContent = "#";
-	icon.classList.add("icon");
+	icon.textContent = "tag";
+	icon.classList.add("material-symbols-outlined", "icon");
+
+	const nameSpan = document.createElement("span");
+	nameSpan.classList.add("text");
+	nameSpan.textContent = project.name;
 
 	const projectButton = document.createElement("button");
 	projectButton.addEventListener("click", () => {
 		onClick(project);
 	});
 	projectButton.setAttribute("data-key", project.id);
-	projectButton.append(icon, project.name);
+	projectButton.append(icon, nameSpan);
 
 	projectElement.replaceChildren(projectButton);
 	return projectElement;
